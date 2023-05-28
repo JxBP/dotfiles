80e894fb92388cbeb5c67c8334f2af4280dc1009
JxBP <61627369+JxBP@users.noreply.github.com>
2023-05-28 11:46:53 +0200

    Disable nvim bufferline

--- nvim/lua/jxbp/plugins/bufferline.lua
+++ nvim/lua/jxbp/plugins/bufferline.lua
@@ -1,4 +1,5 @@
 return {
+	--[[
 	{
 		"akinsho/bufferline.nvim",
 		config = function()
@@ -9,8 +10,16 @@ return {
 						bufferline.style_preset.no_italic,
 						bufferline.style_preset.no_bold,
 					},
+					show_close_icon = false,
+					show_buffer_close_icons = false,
+					diagnostics = "nvim_lsp",
+					diagnostics_indicator = function(count, level)
+						local icon = level:match("error") and " " or ""
+						return " " .. icon .. count
+					end,
 				},
 			})
 		end,
 	},
+    ]]
 }
